78fa8715dbcef3fde674441c2332fdc13f0d79da
Bill Mitchell <wkmitch@gmail.com>
2019-08-24 06:38:21 -0700

    Minutes refactored

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,83 @@ extern crate chrono;
 extern crate pancurses;
 
 use chrono::Local;
-use chrono::{Datelike, Timelike, Utc, NaiveDate};
-use chrono::prelude::*;
+use chrono::Timelike;
 use pancurses::*;
 use std::time::Duration;
 use std::thread::sleep;
 
 const SECONDS_IN_MINUTE: u32 = 60;
-const SECONDS_IN_HOUR: u32 = 3600;
-const SECONDS_IN_DAY: u32 = 86400;
-const SECONDS_IN_YEAR: u32 = 31536000;
+
+struct TimeProgressBar {
+    width: i32,
+    percentage: f64,
+    prefix: String
+}
+
+struct TimeProgressBarWindow<'a> {
+    window: &'a Window,
+    progress_bar: TimeProgressBar
+}
+
+impl<'a> TimeProgressBarWindow<'a> {
+
+    pub fn new(window: &'a Window, progress_bar: TimeProgressBar) -> TimeProgressBarWindow<'a> {
+        let new_window = TimeProgressBarWindow { window: &window, progress_bar: progress_bar };
+        new_window.window.color_set(3);
+        new_window.window.mv(0, 0);
+        new_window
+    }
+
+    fn draw(&self) {
+        self.window.printw(&self.progress_bar.prefix);
+        for n in 1..self.progress_bar.width {
+            if (n as f64 / self.progress_bar.width as f64 * 100.0) < self.progress_bar.percentage as f64 {
+                self.window.printw("█");
+                continue;
+            } 
+            self.window.printw("░");
+        }
+        let formatted_number = format!("{:0>5.*}", 5, format!("{:.*}", 2, self.progress_bar.percentage));
+        self.window.printw(" ");
+        self.window.printw(formatted_number.to_string());
+        self.window.refresh();
+        self.window.clear();
+    }
+}
 
 fn main() {
+    let window = setup_main_window();
+    // https://en.wikipedia.org/wiki/Geometric_Shapes
+    // https://en.wikipedia.org/wiki/Box_Drawing_(Unicode_block)
+    loop {
+
+        // Start Minutes
+        let minutes_bar = TimeProgressBar { 
+            width: window.get_max_x() - 3 - 15,
+            percentage: get_percentage_minute_left(),
+            prefix: String::from(" M ")
+        };
+
+
+
+
+        let time_progress_window = newwin(6, window.get_max_x() - 3, 2, 1);
+        let progress_bar_window = TimeProgressBarWindow::new(&time_progress_window, minutes_bar);
+
+
+
+        window.refresh();
+        window.clear();
+        progress_bar_window.draw();
+
+        sleep(Duration::from_millis(50));
+   }
+  endwin();
+}
+
+fn setup_main_window() -> Window {
     let window = initscr();
-    let max_x: i32 = window.get_max_x();
-    let time_progress_window = newwin(6, max_x - 3, 2, 1);
+    window.color_set(1);
 
     if has_colors() {
         start_color();
@@ -26,185 +88,12 @@ fn main() {
         init_pair(3, COLOR_WHITE, COLOR_BLACK);
         init_pair(4, COLOR_WHITE, COLOR_BLACK);
     }
-
-    // https://en.wikipedia.org/wiki/Geometric_Shapes
-    // https://en.wikipedia.org/wiki/Box_Drawing_(Unicode_block)
-    loop {
-        let date = Local::now();
-        let now = Utc::now();
-
-        window.color_set(1);
-
-        let top_border = format!("{}", date.format("%H:%M:%S - %A %B %d, %Y").to_string());
-        let horizontal_border = "━".repeat((max_x -2) as usize);
-        let top_border = date.format("%H:%M:%S - %A %B %d, %Y").to_string();
-        let border = format!("{: ^1$}", top_border.to_string(), max_x as usize);
-
-        let horizontal_border = "━".repeat((max_x -2) as usize);
-        window.printw(&border);
-        window.mv(1, 0);
-        window.color_set(2);
-
-        window.printw("┃");
-        window.printw(" ".repeat((max_x -2) as usize));
-        window.printw("┃");
-
-        window.printw("┃");
-        window.printw(" ".repeat((max_x -2) as usize));
-        window.printw("┃");
-
-        window.printw("┃");
-        window.printw(" ".repeat((max_x -2) as usize));
-        window.printw("┃");
-
-        window.printw("┃");
-        window.printw(" ".repeat((max_x -2) as usize));
-        window.printw("┃");
-
-        window.printw("┃");
-        window.printw(" ".repeat((max_x -2) as usize));
-        window.printw("┃");
-
-        window.printw("┃");
-        window.printw(" ".repeat((max_x -2) as usize));
-        window.printw("┃");
-
-        window.printw("┃");
-        window.printw(" ".repeat((max_x -2) as usize));
-        window.printw("┃");
-
-        time_progress_window.color_set(3);
-
-        // Start days left
-        let month_length_days = vec![31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-        let today_date: DateTime<Utc> = Utc::now(); 
-        let this_month = today_date.month()-1;
-        let this_day = today_date.day();
-        let mut month_ticker: usize = 0;
-        let mut day_ticker = 0;
-        while month_ticker < this_month as usize {
-            day_ticker = day_ticker + month_length_days[month_ticker];
-            month_ticker+=1;
-        }
-        day_ticker = day_ticker + this_day;
-        let status_bar_data = format!(" It is day {} of 365 - {} days remaining in {}", day_ticker, 365-day_ticker, today_date.year());
-        let status_bar = format!("{: ^1$}", status_bar_data.to_string(), max_x as usize);
-        window.color_set(1);
-        window.printw(&status_bar);
-
-        // Start Minutes
-        time_progress_window.mv(0, 0);
-        time_progress_window.printw(" M ");
-        let progress_width = time_progress_window.get_max_x() - 15;
-        let milli_seconds = (date.timestamp_subsec_millis() as f64/ 1000.0) as f64;
-        let seconds = date.second() as f64 + milli_seconds;
-
-        let minute_progress_percentage_complete : f64 = seconds / SECONDS_IN_MINUTE as f64 * 100.00;
-        for n in 1..progress_width {
-            if (n as f64 / progress_width as f64 * 100.0) < minute_progress_percentage_complete as f64 {
-                time_progress_window.printw("█");
-            } else {
-                time_progress_window.printw("░");
-            }
-        }
-        let formatted_number = format!("{:0>5.*}", 5, format!("{:.*}", 2, minute_progress_percentage_complete));
-        time_progress_window.printw(" ");
-        time_progress_window.printw(formatted_number.to_string());
-
-        // Start Hours
-        time_progress_window.mv(1, 0);
-        time_progress_window.printw(" H ");
-        let progress_width = time_progress_window.get_max_x() - 15;
-        let minutes = (date.minute() as f64 * 60.0) + seconds;
-
-        let hour_progress_percentage_complete = minutes / SECONDS_IN_HOUR as f64 * 100.00;
-        for n in 1..progress_width {
-            if (n as f64 / progress_width as f64 * 100.0) < hour_progress_percentage_complete as f64 {
-                time_progress_window.printw("█");
-            } else {
-                time_progress_window.printw("░");
-            }
-        }
-        let formatted_number = format!("{:0>5.*}", 5, format!("{:.*}", 2, hour_progress_percentage_complete));
-        time_progress_window.printw(" ");
-        time_progress_window.printw(formatted_number.to_string());
-
-
-        // Start Days
-        time_progress_window.mv(2, 0);
-        time_progress_window.printw(" D ");
-        let progress_width = time_progress_window.get_max_x() - 15;
-        let days = (date.hour() as f64 * 3600.0) + minutes + seconds;
-
-        let day_progress_percentage_complete = days / SECONDS_IN_DAY as f64 * 100.00;
-        for n in 1..progress_width {
-            if (n as f64 / progress_width as f64 * 100.0) < day_progress_percentage_complete as f64 {
-                time_progress_window.printw("█");
-            } else {
-                time_progress_window.printw("░");
-            }
-        }
-        let formatted_number = format!("{:0>5.*}", 5, format!("{:.*}", 2, day_progress_percentage_complete));
-        time_progress_window.printw(" ");
-        time_progress_window.printw(formatted_number.to_string());
-
-        // Start Months
-        time_progress_window.mv(3, 0);
-        time_progress_window.printw(" M ");
-
-        let progress_width = time_progress_window.get_max_x() - 15;
-        let seconds_in_current_month = seconds_in_month(now.year(), now.month());
-        let seconds_elapsed_in_current_month = (date.day() as f64 * 86400.0) + days + minutes + seconds;
-
-        let month_progress_percentage_complete = seconds_elapsed_in_current_month / seconds_in_current_month as f64 * 100.00;
-        for n in 1..progress_width {
-            if (n as f64 / progress_width as f64 * 100.0) < month_progress_percentage_complete as f64 {
-                time_progress_window.printw("█");
-            } else {
-                time_progress_window.printw("░");
-            }
-        }
-        let formatted_number = format!("{:0>5.*}", 5, format!("{:.*}", 2, month_progress_percentage_complete));
-        time_progress_window.printw(" ");
-        time_progress_window.printw(formatted_number.to_string());
-
-        // Start Year
-        time_progress_window.mv(4, 0);
-        time_progress_window.printw(" Y ");
-
-        let progress_width = time_progress_window.get_max_x() - 15;
-        let days = (date.hour() as f64 * 3600.0) + minutes + seconds;
-        let seconds_elapsed_in_current_year = (day_ticker as f64 * SECONDS_IN_DAY as f64) + days;
-
-
-        let year_progress_percentage_complete = seconds_elapsed_in_current_year / SECONDS_IN_YEAR as f64 * 100.00;
-        for n in 1..progress_width {
-            if (n as f64 / progress_width as f64 * 100.0) < year_progress_percentage_complete as f64 {
-                time_progress_window.printw("█");
-            } else {
-                time_progress_window.printw("░");
-            }
-        }
-        let formatted_number = format!("{:0>5.*}", 5, format!("{:.*}", 2, year_progress_percentage_complete));
-        time_progress_window.printw(" ");
-        time_progress_window.printw(formatted_number.to_string());
-
-
-        window.refresh();
-        window.clear();
-        time_progress_window.refresh();
-        time_progress_window.clear();
-
-        sleep(Duration::from_millis(50));
-   }
-  endwin();
+    window
 }
 
-fn seconds_in_month(year: i32, month: u32) -> u32 {
-    // the first day of the next month...
-    let (y, m) = if month == 12 { (year + 1, 1) } else { (year, month + 1) };
-    let d = NaiveDate::from_ymd(y, m, 1);
-
-    // ...is preceded by the last day of the original month
-    d.pred().day() * SECONDS_IN_DAY
+fn get_percentage_minute_left() -> f64 {
+        let date = Local::now();
+        let milli_seconds = (date.timestamp_subsec_millis() as f64/ 1000.0) as f64;
+        let seconds = date.second() as f64 + milli_seconds;
+        seconds / SECONDS_IN_MINUTE as f64 * 100.00
 }
