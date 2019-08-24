338f970bfb641fa13c49d5c01fc6811bd057ca84
Bill Mitchell <wkmitch@gmail.com>
2019-08-24 06:53:21 -0700

    hours refactored

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use std::time::Duration;
 use std::thread::sleep;
 
 const SECONDS_IN_MINUTE: u32 = 60;
+const SECONDS_IN_HOUR: u32 = 3600;
+
 
 struct TimeProgressBar {
     width: i32,
@@ -17,30 +19,35 @@ struct TimeProgressBar {
 
 struct TimeProgressBarWindow<'a> {
     window: &'a Window,
-    progress_bar: TimeProgressBar
+    progress_bars: Vec<TimeProgressBar>
 }
 
 impl<'a> TimeProgressBarWindow<'a> {
 
-    pub fn new(window: &'a Window, progress_bar: TimeProgressBar) -> TimeProgressBarWindow<'a> {
-        let new_window = TimeProgressBarWindow { window: &window, progress_bar: progress_bar };
+    pub fn new(window: &'a Window, progress_bars: Vec<TimeProgressBar>) -> TimeProgressBarWindow<'a> {
+        let new_window = TimeProgressBarWindow { window: &window, progress_bars: progress_bars };
         new_window.window.color_set(3);
         new_window.window.mv(0, 0);
         new_window
     }
 
     fn draw(&self) {
-        self.window.printw(&self.progress_bar.prefix);
-        for n in 1..self.progress_bar.width {
-            if (n as f64 / self.progress_bar.width as f64 * 100.0) < self.progress_bar.percentage as f64 {
-                self.window.printw("█");
-                continue;
-            } 
-            self.window.printw("░");
+        for (i, progress_bar) in self.progress_bars.iter().enumerate() {
+            self.window.printw(&progress_bar.prefix);
+            for n in 1..progress_bar.width {
+                if (n as f64 / progress_bar.width as f64 * 100.0) < progress_bar.percentage as f64 {
+                    self.window.printw("█");
+                    continue;
+                } 
+                self.window.printw("░");
+            }
+            let formatted_number = format!("{:0>5.*}", 5, format!("{:.*}", 2, progress_bar.percentage));
+            self.window.printw(" ");
+            self.window.printw(formatted_number.to_string());
+            self.window.mv((i + 1) as i32, 0);
+
         }
-        let formatted_number = format!("{:0>5.*}", 5, format!("{:.*}", 2, self.progress_bar.percentage));
-        self.window.printw(" ");
-        self.window.printw(formatted_number.to_string());
+
         self.window.refresh();
         self.window.clear();
     }
@@ -52,18 +59,27 @@ fn main() {
     // https://en.wikipedia.org/wiki/Box_Drawing_(Unicode_block)
     loop {
 
-        // Start Minutes
         let minutes_bar = TimeProgressBar { 
             width: window.get_max_x() - 3 - 15,
             percentage: get_percentage_minute_left(),
             prefix: String::from(" M ")
         };
+        
+        let hours_bar = TimeProgressBar { 
+            width: window.get_max_x() - 3 - 15,
+            percentage: get_percentage_hour_left(),
+            prefix: String::from(" H ")
+        };
+
+        // Start Hours
+        // time_progress_window.mv(1, 0);
+
 
 
 
 
         let time_progress_window = newwin(6, window.get_max_x() - 3, 2, 1);
-        let progress_bar_window = TimeProgressBarWindow::new(&time_progress_window, minutes_bar);
+        let progress_bar_window = TimeProgressBarWindow::new(&time_progress_window, vec!(minutes_bar, hours_bar));
 
 
 
@@ -96,4 +112,15 @@ fn get_percentage_minute_left() -> f64 {
         let milli_seconds = (date.timestamp_subsec_millis() as f64/ 1000.0) as f64;
         let seconds = date.second() as f64 + milli_seconds;
         seconds / SECONDS_IN_MINUTE as f64 * 100.00
+}
+
+fn get_percentage_hour_left() -> f64 {
+        let date = Local::now();
+        let milli_seconds = (date.timestamp_subsec_millis() as f64/ 1000.0) as f64;
+        let seconds = date.second() as f64 + milli_seconds;
+        let minutes = (date.minute() as f64 * 60.0) + seconds;
+
+        let milli_seconds = (date.timestamp_subsec_millis() as f64/ 1000.0) as f64;
+        let seconds = date.second() as f64 + milli_seconds;
+        minutes / SECONDS_IN_HOUR as f64 * 100.00
 }
