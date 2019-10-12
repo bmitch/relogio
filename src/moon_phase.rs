7e923968fc42ac8c0023df0540784c22696cd050
Robby Stokoe <robbystk@gmail.com>
2019-10-12 16:12:52 -0600

    Use reqwest_mock to test without hitting USNO

--- src/moon_phase.rs
+++ src/moon_phase.rs
@@ -1,19 +1,34 @@
 use chrono::{Date, TimeZone, Utc};
+use reqwest_mock::{Client, DirectClient};
 use serde_json::{from_str, Value};
 use std::error::Error;
 use std::fmt;
 
 fn get_moon_phase(dt: Date<Utc>) -> MoonPhase {
-    let moon_json = get_usno_json(dt).expect("Problem getting data from USNO");
+    let moon_json = new_client()
+        .get_usno_json(dt)
+        .expect("Problem getting data from USNO");
     process_moon_data(&moon_json).expect("Problem trying to parse json")
 }
 
-fn get_usno_json(dt: Date<Utc>) -> reqwest::Result<String> {
-    let url = dt
-        .format("https://api.usno.navy.mil/moon/phase?date=%m/%d/%Y&nump=1")
-        .to_string();
+// An abstraction of a reqwest client that may hold a real client or a stub
+struct MyClient<C: Client> {
+    client: C,
+}
 
-    reqwest::get(&url)?.text()
+impl<C: Client> MyClient<C> {
+    fn get_usno_json(&self, dt: Date<Utc>) -> Result<String, reqwest_mock::error::Error> {
+        let url = dt
+            .format("https://api.usno.navy.mil/moon/phase?date=%m/%d/%Y&nump=1")
+            .to_string();
+
+        self.client.get(&url).send()?.body_to_utf8()
+    }
+}
+
+// Use a DirectClient for the actual application
+fn new_client() -> MyClient<DirectClient> {
+    MyClient { client: DirectClient::new() }
 }
 
 #[derive(Debug)]
@@ -63,6 +78,43 @@ enum MoonPhase {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use reqwest_mock::{Method, StubClient, StubDefault, StubSettings, StubStrictness, Url};
+
+    // A stub client for testing using the March 2019 Supermoon
+    fn test_client() -> MyClient<StubClient> {
+        let mut client = StubClient::new(StubSettings {
+            default: StubDefault::Error,
+            strictness: StubStrictness::MethodUrl,
+        });
+        assert!(client
+            .stub(
+                Url::parse("https://api.usno.navy.mil/moon/phase?date=03/17/2019&nump=1").unwrap()
+            )
+            .method(Method::GET)
+            .response()
+            .body(
+                "{
+      \"error\":false,
+      \"apiversion\":\"2.2.1\",
+      \"year\":2019,
+      \"month\":3,
+      \"day\":20,
+      \"numphases\":1,
+      \"datechanged\":false,
+      \"phasedata\":[
+            {
+               \"phase\":\"Full Moon\",
+               \"date\":\"2019 Mar 21\",
+               \"time\":\"01:43\"
+            }
+      ]
+   }"
+            )
+            .mock()
+            .is_ok());
+
+        MyClient { client: client }
+    }
 
     #[test]
     // this is a slow test that actually hits the USNO api
@@ -70,6 +122,11 @@ mod tests {
     #[ignore]
     fn test_get_moon_data() {
         get_moon_phase(Utc.ymd(2019, 03, 17));
+    }
+
+    #[test]
+    fn test_get_json_data_for_given_date() {
+        assert!(test_client().get_usno_json(Utc.ymd(2019, 03, 17)).is_ok());
     }
 
     #[test]
